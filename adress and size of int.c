2f6df298d6fd938bd50221b9a2c145ecca3fe404
ayeshaaa234 <ayeshashahzad386@gmail.com>
2023-04-17 17:48:00 +0500

    pushpull

---
+++ adress and size of int.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+int no=10;
+
+
+
+printf("value of no=%d\n",no);
+printf("adress of no=%d\n",&no);
+printf("size of no=%d bytes\n",sizeof(no));
+
+
+
+
+
+
+return 0;}
